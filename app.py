6afd940ef273c418d6e148cd7620aebb669f8803
nikhil2297 <nikhillohar2297@gmail.com>
2024-11-26 10:42:10 -0500

    Added health check

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello, This is nikhil Lohar! Task 2 - 100925168"
 
+@app.route('/health-check')
+def hello():
+    return "This is a health check"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
